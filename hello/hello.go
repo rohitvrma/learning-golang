0585c5674fb134d49a403eec98199da786c0b6e8
Rohit Verma <xlrohit@gmail.com>
2017-02-02 04:28:26 +0530

    using package and testing

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rohitvrma/learning-golang/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, World.\n")
+	fmt.Println(stringutil.Reverse("Hello, World!"))
 }
